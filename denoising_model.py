7373b014b97c0702ae0732f69f6634314f97f281
inzapp <inzapp@naver.com>
2023-10-30 14:10:02 +0900

    split checkpoint manager to class

--- denoising_model.py
+++ denoising_model.py
@@ -40,6 +40,7 @@ from time import time
 from model import Model
 from generator import DataGenerator
 from lr_scheduler import LRScheduler
+from ckpt_manager import CheckpointManager
 
 
 class TrainingConfig:
@@ -72,7 +73,7 @@ class TrainingConfig:
         self.training_view = training_view
 
 
-class DenoisingModel:
+class DenoisingModel(CheckpointManager):
     def __init__(self, config, training):
         assert config.save_interval >= 1000
         self.pretrained_model_path = config.pretrained_model_path
@@ -81,7 +82,6 @@ class DenoisingModel:
         self.input_type = config.input_type
         self.user_input_shape = (config.input_rows, config.input_cols, 3 if config.input_type == 'rgb' else 1)
         self.model_input_shape = self.convert_to_model_input_shape(self.user_input_shape)
-        self.model_name = config.model_name
         self.lr = config.lr
         self.warm_up = config.warm_up
         self.batch_size = config.batch_size
@@ -89,9 +89,8 @@ class DenoisingModel:
         self.iterations = config.iterations
         self.training_view = config.training_view
         warnings.filterwarnings(action='ignore')
+        self.set_model_name(config.model_name)
 
-        self.checkpoint_path = None
-        self.pretrained_iteration_count = 0
         self.live_view_previous_time = time()
         if not training:
             self.set_global_seed()
@@ -119,6 +118,7 @@ class DenoisingModel:
             print(f'no noisy images found in {self.validation_image_path}')
             exit(0)
 
+        self.pretrained_iteration_count = 0
         if self.pretrained_model_path != '':
             self.model, self.user_input_shape, self.model_input_shape = self.load_model(self.pretrained_model_path)
             self.pretrained_iteration_count = self.parse_pretrained_iteration_count(self.pretrained_model_path)
@@ -144,35 +144,6 @@ class DenoisingModel:
         np.random.seed(seed)
         os.environ['PYTHONHASHSEED'] = str(seed)
         tf.random.set_seed(seed)
-
-    def parse_pretrained_iteration_count(self, pretrained_model_path):
-        iteration_count = 0
-        sp = f'{os.path.basename(pretrained_model_path)[:-3]}'.split('_')
-        for i in range(len(sp)):
-            if sp[i] == 'iter' and i > 0:
-                try:
-                    iteration_count = int(sp[i-1])
-                except:
-                    pass
-                break
-        return iteration_count
-
-    def make_checkpoint_dir(self):
-        os.makedirs(self.checkpoint_path, exist_ok=True)
-
-    def init_checkpoint_dir(self):
-        inc = 0
-        while True:
-            if inc == 0:
-                new_checkpoint_path = f'checkpoint/{self.model_name}'
-            else:
-                new_checkpoint_path = f'checkpoint/{self.model_name}_{inc}'
-            if os.path.exists(new_checkpoint_path) and os.path.isdir(new_checkpoint_path):
-                inc += 1
-            else:
-                break
-        self.checkpoint_path = new_checkpoint_path
-        self.make_checkpoint_dir()
 
     def is_valid_path(self, path):
         return os.path.exists(path) and os.path.isdir(path)
@@ -426,17 +397,6 @@ class DenoisingModel:
         avg_ssim = ssim_sum / float(len(image_paths_gt))
         print(f'\npsnr : {avg_psnr:.2f}, ssim : {avg_ssim:.4f}')
 
-    def save_model(self, iteration_count):
-        self.make_checkpoint_dir()
-        save_path = f'{self.checkpoint_path}/model_{iteration_count}_iter.h5'
-        self.model.save(save_path, include_optimizer=False)
-        backup_path = f'{save_path}.bak'
-        sh.move(save_path, backup_path)
-        for last_model_path in glob(f'{self.checkpoint_path}/model_*_iter.h5'):
-            os.remove(last_model_path)
-        sh.move(backup_path, save_path)
-        return save_path
-
     def train(self):
         self.model.summary()
         print(f'\ntrain on {len(self.train_image_paths_gt)} gt, {len(self.train_image_paths_noisy)} noisy samples.')
@@ -456,7 +416,7 @@ class DenoisingModel:
                 if self.training_view:
                     self.training_view_function()
                 if iteration_count % 2000 == 0:
-                    self.save_model(iteration_count)
+                    self.save_last_model(self.model, iteration_count)
                 if iteration_count == self.iterations:
                     print('\ntrain end successfully')
                     return
